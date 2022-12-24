34993eeefc4663d9143939d39629ffb79a5a4bb8
JustDMare <just@dmare.me>
2022-12-24 12:45:43 +0100

    Finished basics for the Note Schema

--- src/models/Note.ts
+++ src/models/Note.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema, model, connect } from "mongoose";
-import { Block, BlockType, BlockUniqueProperties, BLOCK_TYPES } from "vnotes-types";
+import { Block, BlockUniqueProperties, BLOCK_TYPES } from "vnotes-types";
 import { Note } from "vnotes-types";
 
 const blockSchema = new Schema<Block>({
@@ -10,23 +10,24 @@ const blockSchema = new Schema<Block>({
 	lastUpdatedTime: { type: String, required: true },
 	content: { type: String },
 	uniqueProperties: {
-		type: { selected: { type: Boolean, required: isSelectedRequired, default: false } },
-		required: isSelectedRequired,
+		type: { selected: { type: Boolean, required: false, default: false } },
+		required: true,
 	},
 });
 
 const noteSchema = new Schema<Note>({
 	noteID: mongoose.Types.ObjectId,
-	parentID: mongoose.Types.ObjectId,
+	parentID: { type: mongoose.Types.ObjectId, required: false },
 	title: { type: String, required: true },
 	createdTime: { type: String, required: true },
 	lastUpdatedTime: { type: String, required: true },
 	content: [blockSchema],
 });
 
-function isSelectedRequired(this: Block) {
-	if (this.type === "checkbox") {
-		return true;
+blockSchema.pre("validate", function (next) {
+	if (this.type === "checkbox" && !Object.hasOwn(this.uniqueProperties, "selected")) {
+		const error = new Error('"selected" property is required for checkbox blocks');
+		next(error);
 	}
-	return false;
-}
+	next();
+});
