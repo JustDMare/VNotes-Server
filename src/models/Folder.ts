1ce72b4a3217914dbed6bb443b8a3ab0409a69b0
JustDMare <just@dmare.me>
2022-12-26 20:13:50 +0100

    Fixed indexing bug on subdocument

--- src/models/Folder.ts
+++ src/models/Folder.ts
@@ -2,7 +2,7 @@ import mongoose, { Schema } from "mongoose";
 import { FolderSchema } from "vnotes-types";
 
 const folderSchema = new Schema<FolderSchema>({
-	folderID: { type: String, required: true, unique: true, index: 1 },
+	folderID: { type: String, required: true, unique: true },
 	parentID: { type: String, required: false },
 	name: { type: String, required: true },
 	createdTime: { type: String, required: true },
