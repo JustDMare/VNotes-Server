0b9b1421f5e1f1a9c5df31724982c330e81aef36
JustDMare <just@dmare.me>
2023-01-12 18:00:54 +0100

    Reordered note routes

--- src/routes/note-routes.ts
+++ src/routes/note-routes.ts
@@ -2,11 +2,12 @@ import { noteController } from "../controllers/note-controller";
 import express from "express";
 
 const router = express.Router();
-router.get("/:id", noteController.findNote);
+
 router.post("/create", noteController.createNote);
-router.delete("/delete/:id", noteController.deleteNote);
 router.post("/update-content", noteController.updateNoteContent);
 router.post("/rename", noteController.updateNoteTitle);
 router.post("/move", noteController.updateNoteParentId);
+router.delete("/delete/:id", noteController.deleteNote);
+router.get("/:id", noteController.findNote);
 
 export default router;
