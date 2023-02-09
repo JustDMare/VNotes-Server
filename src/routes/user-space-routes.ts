a75a769a0ce9c79cc412505c13f710dcae9293ab
JustDMare <just@dmare.me>
2023-02-09 19:40:46 +0100

    Minor change to userSpace delete route

--- src/routes/user-space-routes.ts
+++ src/routes/user-space-routes.ts
@@ -4,7 +4,7 @@ import { userSpaceController } from "../controllers/user-space-controller";
 const router = express.Router();
 
 router.post("/create", userSpaceController.createUserSpace);
-router.delete("/delete/", userSpaceController.deteleUserSpace);
+router.delete("/delete", userSpaceController.deteleUserSpace);
 router.get("/:locale", userSpaceController.findAllUserSpaceContent);
 
 export default router;
