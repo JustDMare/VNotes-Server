aaec76d5f907abd47e9d5861580ab280bec39363
JustDMare <just@dmare.me>
2023-03-16 19:42:43 +0100

    Created breadcrump functionality, but doing it in the same API call takes too much time (almost 0.5 sec)

--- src/controllers/note-controller.ts
+++ src/controllers/note-controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import mongoose, { Types } from "mongoose";
+import { FolderModel } from "../models/Folder";
 import { NoteModel } from "../models/Note";
 import { UserSpaceModel } from "../models/UserSpace";
 import { checkFolderExists, checkValidObjetId } from "./common-helpers";
@@ -8,13 +9,39 @@ import { checkFolderExists, checkValidObjetId } from "./common-helpers";
  * Retrieves the note whose `_id` matches the `id` given as a parameter.
  * @returns note whose `_id` matches the `id` given as a parameter.
  */
-function findNote(req: Request, res: Response, next: NextFunction) {
-  return NoteModel.findById(req.params.id)
-    .then((note) =>
-      //TODO: Check all the status messages in case they can be done better
-      note ? res.status(201).json({ note }) : res.status(404).json("Note not found")
-    )
-    .catch((error) => res.status(500).json({ error: error.message }));
+async function findNote(req: Request, res: Response, next: NextFunction) {
+  const noteId = req.params.id;
+  try {
+    checkValidObjetId(noteId);
+  } catch (error) {
+    return res.status(400).json({ error: (<Error>error).message });
+  }
+  try {
+    const note = await NoteModel.findById(req.params.id);
+    if (!note) {
+      return res.status(404).json("Note not found");
+    }
+    const breadcrumbs: mongoose.Document[] = [];
+    if (note.parentId) {
+      const parentId = note.parentId.toString();
+      await generateNoteBreadcrumbs(parentId, breadcrumbs);
+    }
+
+    return res.status(201).json({ note, breadcrumbs });
+  } catch (error) {
+    res.status(500).json({ error: (<Error>error).message });
+  }
+}
+//Either do this in a separate endpoint or do it in the frontend
+async function generateNoteBreadcrumbs(parentId: string, breadcrumbs: mongoose.Document[]) {
+  await checkFolderExists(parentId);
+  const parentFolder = await FolderModel.findById(parentId);
+  if (parentFolder) {
+    breadcrumbs.push(parentFolder);
+    if (parentFolder.parentId) {
+      await generateNoteBreadcrumbs(parentFolder.parentId.toString(), breadcrumbs);
+    }
+  }
 }
 
 /**
